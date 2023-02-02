d724f27fb031c095cdf9d64c968e844281905c29
hbin007 <rlawnsxo10@naver.com>
2023-02-02 15:39:47 +0900

    receivedUser

--- src/main/java/com/example/wero/core/receiveduser/domain/ReceivedUser.java
+++ src/main/java/com/example/wero/core/receiveduser/domain/ReceivedUser.java
@@ -1,4 +1,34 @@
 package com.example.wero.core.receiveduser.domain;
 
+import com.example.wero.core.user.domain.User;
+import lombok.*;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@ToString
+@Entity
+@Table(name = "ReceivedUsers")
 public class ReceivedUser {
+
+    @Id
+    @Column(name = "userId")
+    private String userId;
+
+    @ManyToOne(targetEntity = User.class)
+    @JoinColumn(name="user_nickname")
+    private User user;
+    @Column(name = "user_nickname", insertable = false, updatable = false)
+    private String writerNickName;
+
+    private String myLetterTitle;
+
+    private String letterReceivedWhen;
+
+    private boolean isRead;
+
+
 }
--- src/main/java/com/example/wero/core/receiveduser/domain/ReceivedUserDTO.java
+++ src/main/java/com/example/wero/core/receiveduser/domain/ReceivedUserDTO.java
@@ -1,4 +1,44 @@
 package com.example.wero.core.receiveduser.domain;
 
+
+import com.example.wero.core.user.domain.User;
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReceivedUserDTO {
+
+    @NotBlank
+    private String userId;
+
+    private String writerNickName;
+
+    @NotBlank
+    private String myLetterTitle;
+
+    private String letterReceivedWhen;
+
+    @NotNull
+    private boolean isRead;
+
+    public ReceivedUser receivedUser(ReceivedUserDTO receivedUserDTO){
+        return ReceivedUser.builder()
+                .userId(userId)
+                .writerNickName(writerNickName)
+                .myLetterTitle(myLetterTitle)
+                .letterReceivedWhen(letterReceivedWhen)
+                .isRead(isRead)
+                .build();
+    }
+
 }
