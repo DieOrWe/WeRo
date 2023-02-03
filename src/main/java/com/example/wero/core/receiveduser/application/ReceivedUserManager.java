e70e14ce3a4bf8a346363940cdbb0196a87f5537
The6thMonthJune <ttkd16@gmail.com>
2023-02-03 11:20:49 +0900

    working on receiveUser

--- src/main/java/com/example/wero/core/receiveduser/application/ReceivedUserManager.java
+++ src/main/java/com/example/wero/core/receiveduser/application/ReceivedUserManager.java
@@ -1,4 +1,20 @@
 package com.example.wero.core.receiveduser.application;
 
-public class ReceivedUserManager {
+import com.example.wero.core.receiveduser.infrastructure.ReceivedUserRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReceivedUserManager implements ReceivedUserFinder, ReceivedUserEditor{
+    private final ReceivedUserRepository receivedUserRepository;
+
+    private final ModelMapper modelMapper;
+
+    public ReceivedUserManager(ReceivedUserRepository receivedUserRepository, ModelMapper modelMapper){
+        this.receivedUserRepository = receivedUserRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public String
 }
