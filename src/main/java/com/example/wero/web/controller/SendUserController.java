97b86822218b4ceddc94975c4a88ff14dc0d45a8
heon98 <duafogjs@naver.com>
2023-02-03 16:30:08 +0900

    findSenderUserLetter

--- src/main/java/com/example/wero/web/controller/SendUserController.java
+++ src/main/java/com/example/wero/web/controller/SendUserController.java
@@ -27,7 +27,7 @@ public class SendUserController {
 
     @PostMapping(path = "/check/{myLetterId}")
     public MyLetterDTO findMySendLetter(@PathVariable String myLetterId) {
-        return null;
+        return finder.findSendLetter(myLetterId);
     }
     
     @DeleteMapping
