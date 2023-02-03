57e598e61a100e0bf0bc0f6ee821c2c20dd8c0af
The6thMonthJune <ttkd16@gmail.com>
2023-02-03 12:46:47 +0900

    worked on ReceivedLetter

--- src/main/java/com/example/wero/core/receiveduser/application/ReceivedUserManager.java
+++ src/main/java/com/example/wero/core/receiveduser/application/ReceivedUserManager.java
@@ -1,8 +1,14 @@
 package com.example.wero.core.receiveduser.application;
 
+import com.example.wero.core.myletter.domain.MyLetterDTO;
+import com.example.wero.core.receiveduser.domain.ReceivedUser;
+import com.example.wero.core.receiveduser.domain.ReceivedUserDTO;
 import com.example.wero.core.receiveduser.infrastructure.ReceivedUserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ReceivedUserManager implements ReceivedUserFinder, ReceivedUserEditor{
@@ -16,5 +22,15 @@ public class ReceivedUserManager implements ReceivedUserFinder, ReceivedUserEdit
     }
 
     @Override
-    public String
+    public List<ReceivedUserDTO> findAllMyReceivedLetters(String userId){
+        List<ReceivedUser> foundUser = receivedUserRepository.findAll();
+        List<ReceivedUserDTO> result;
+        result = foundUser.stream().map(p -> modelMapper.map(p,ReceivedUserDTO.class)).collect(Collectors.toList());
+        return result;
+    }
+
+    @Override
+    public MyLetterDTO findSendLetter(String myLetterId) {
+        return null;
+    }
 }
--- src/main/java/com/example/wero/core/receiveduser/infrastructure/ReceivedUserRepository.java
+++ src/main/java/com/example/wero/core/receiveduser/infrastructure/ReceivedUserRepository.java
@@ -4,6 +4,11 @@ import com.example.wero.core.receiveduser.domain.ReceivedUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ReceivedUserRepository extends JpaRepository<ReceivedUser, String> {
+    Optional<ReceivedUser> findByMyLetterId(String letterId);
+
+    Optional<ReceivedUser> deleteByMyLetterId(String letterId);
 }
