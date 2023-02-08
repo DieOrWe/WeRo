2a01b23db37f0fd71f088983e7fad27387d795fd
hbin007 <rlawnsxo10@naver.com>
2023-02-08 17:12:12 +0900

    myletterSocket

--- src/main/java/com/example/wero/web/controller/GreetingController.java
+++ src/main/java/com/example/wero/web/controller/GreetingController.java
@@ -1,6 +1,6 @@
 package com.example.wero.web.controller;
 
-import com.example.wero.core.myletter.domain.MyLetter;
+import com.example.wero.core.myletter.application.MyLetterManager;
 import com.example.wero.core.websocket.Greeting;
 import com.example.wero.core.websocket.HelloMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -11,13 +11,20 @@ import org.springframework.web.util.HtmlUtils;
 
 @Controller
 public class GreetingController {
+    private final MyLetterManager myLetterManager;
+
+    public GreetingController(MyLetterManager myLetterManager) {
+        this.myLetterManager = myLetterManager;
+    }
+
 
 
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
-    public Greeting greeting(MyLetter myLetter) throws Exception {
+    public Greeting greeting(String myLetterId) throws Exception {
+        String[] myLetterIds = myLetterId.substring(2,myLetterId.length()-2).split("\":\"");
         Thread.sleep(1000); // simulated delay
-        return new Greeting("myLetter" + HtmlUtils.htmlEscape(myLetter.getMyLetterContent()) + "@@@");
+        return new Greeting("myLetter" + HtmlUtils.htmlEscape(myLetterManager.findMyLetter(myLetterIds[1]).getMyLetterContent() + "@@@"));
     }
 
 
