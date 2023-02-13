e0a906744a9256d20524136c92782f2df1e4c1ad
heon98 <duafogjs@naver.com>
2023-02-13 11:35:25 +0900

    qwer

--- src/main/java/com/example/wero/core/receiveduser/infrastructure/ReceivedUserRepository.java
+++ src/main/java/com/example/wero/core/receiveduser/infrastructure/ReceivedUserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
@@ -19,6 +20,7 @@ public interface ReceivedUserRepository extends JpaRepository<ReceivedUser, Inte
     @Query(value = "SELECT * FROM RECEIVED_USERS WHERE USER_ID IS NULL", nativeQuery = true)
     List<ReceivedUser> findByUserIdIsNull();
     
+    @Transactional
     Optional<String> deleteByMyLetterId(String myLetterId);
     
     @Query(value = "SELECT USER_ID FROM RECEIVED_USERS", nativeQuery = true)
